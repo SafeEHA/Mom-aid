097b0082a54996d06c4809b9357aad89686b602e
Medina Sheriff <medlin142@gmail.com>
2023-01-17 10:04:13 +0100

    Import community component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import LaunchPage from "./Components/LaunchPage";
+import CommunityPage from "./Components/CommunityPage";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
       <LaunchPage />
+      <CommunityPage/>
     </div>
   );
 }
