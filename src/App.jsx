6b3cf93aa71553de1f9c154cf3a8afe656487b4f
SafeEHA <98215429+SafeEHA@users.noreply.github.com>
2023-01-17 12:40:03 +0100

    work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Routes, Route } from "react-router-dom"
 import './App.css'
 import LoginPage from './components/LoginPage'
-
+import CommunityPostPage from './components/CommunityPostPage'
 function App() {
 
   return (
   <div>
-    <LoginPage />
+    <Routes>
+      <Route element={<LoginPage/>} path="login"/>
+      <Route element={<CommunityPostPage />} path="post"/>
+    
+    </Routes>
+   
   </div>
 
   )
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,40 +1,43 @@
-import React from 'react'
-
+import React from "react";
 
 const LoginPage = () => {
   return (
- <div className="container">
-      
+    <div className="container">
       <div className="wrapper">
-       <div>
-      <h1 className='title'> MOMAID</h1>
-      <p className="welcome-text"> Hello, Welcome back to your account</p>
+        <div>
+          <h1 className="title"> MOMAID</h1>
+        </div>
 
+        <div>
+          <label for="" className="field-title1">
+            Username
+          </label>
+          <br />
+          <input
+            type="text"
+            className="text-field1"
+            placeholder=" not more than 8 characters"
+          ></input>
+        </div>
+
+        <div>
+          <label for="" className="field-title2">
+            Password
+          </label>
+          <br />
+          <input
+            type="text"
+            className="text-field2"
+            placeholder=" 6-10 characters"
+          ></input>
+        </div>
+
+        <button className="btn" type="button">
+          LOGIN
+        </button>
       </div>
-
-      <div >
-      <label for="" className="field-title1">Username</label><br/>
-  <input type="text" className="text-field1" placeholder=" not more than 8 characters"></input>
-</div>
-
-<div>
-      <label for="" className="field-title2">Password</label><br/>
-  <input type="text" className="text-field2"placeholder=" 6-10 characters"></input>
-  <p className="text-buttom">Forgot password?</p>
-</div>
-<div className="checker">
-<input type="checkbox"value=""></input>
-<label for="">Remember me</label>
-</div>
-
-<button className="btn" type="button">LOGIN</button>
-<p className='buttomtext'>Don't have an account yet? <a href="#" className='signup-text'>Signup</a></p>
-
-</div>
-    
     </div>
-
-  )
-}
+  );
+};
 
 export default LoginPage;
