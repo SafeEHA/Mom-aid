114966660587dd1a59d0e71f648996dfa4552082
Medina Sheriff <medlin142@gmail.com>
2023-01-18 21:00:20 +0100

    deleted codes that are not in use

--- src/Components/MoreTopics.jsx
+++ src/Components/MoreTopics.jsx
@@ -4,18 +4,6 @@ const MoreTopics = ({ title, text, lists }) => {
       <div className="MoreTopics">
         <div className="moreTopic__1">
           <h4 className="CommunityPage__heading1">{title}</h4>
-          {/* <p className="CommunityPage__text">
-            {text}
-          </p>
-          <ul className="MoreTopics__list">
-        {lists.map(list=><li key={list}>{list}</li>)}
-           
-          </ul>
-          <button>More</button>
-        </div>
-        <div className="hotTopic__1__image">
-          <img src={feedingBaby} alt="feeding baby" />
-        </div> */}
         </div>
       </div>
     </>
