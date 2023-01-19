a85e23dddd04aaf19d3185bbfea3a40c21e70c3e
Medina Sheriff <medlin142@gmail.com>
2023-01-19 21:20:52 +0100

    created component for post detail

---
+++ src/Components/CommPostDetail.jsx
@@ -0,0 +1,85 @@
+function CommPostDetail() {
+  return (
+    <div className="CommPostDetail">
+      <div>
+        <h3>Post About Breastfeeding</h3>
+        <hr />
+        <ul>
+          <li className="topicTitle">Pumping</li>
+          <div className="nameTag">
+            <div className="nameLogo">M</div>
+            <h6 className="posterName">By MimiCoolMom</h6>
+          </div>
+          <div className="collapsible__content">
+            <p>
+              What size flange will I need for a breast pump? any
+              recommendations on what brand and where to get in Abuja?
+            </p>
+            <i>47 minutes ago by MimiCoolMom</i>
+            <aside>Open thread</aside>
+            <hr />
+          </div>
+          <li className="topicTitle">Hurting Nipples!!!</li>
+          <div className="nameTag">
+            <div className="nameLogo">S</div>
+            <h6 className="posterName">By Syndie</h6>
+          </div>
+          <div className="collapsible__content">
+            <p>
+              No one told me that I needed to mentally prepare for my nipples to
+              hurt this bad. I have been told it would stop, but it's been 6
+              days, and the pain seem to be getting worse. I need hugs.
+            </p>
+            <i>59 minutes ago by Syndie</i>
+            <aside>Open thread</aside>
+            <hr />
+          </div>
+          <li className="topicTitle">Supplimentary Feeding At 2 Weeks?</li>
+          <div className="nameTag">
+            <div className="nameLogo">L</div>
+            <h6 className="posterName">By LeemarhRabs</h6>
+          </div>
+          <div className="collapsible__content">
+            <p>
+              Is it ok to start giving my 2 weeks old baby milk and cereal? He
+              cries all the time, and my yoga instructor said it might be
+              because he is hungry.
+            </p>
+            <i>1 hour ago by LeemarhRabs</i>
+            <aside>Open thread</aside>
+            <hr />
+          </div>
+          <li className="topicTitle">Lactation Cookies</li>
+          <div className="nameTag">
+            <div className="nameLogo">G</div>
+            <h6 className="posterName">By Gloria</h6>
+          </div>
+          <div className="collapsible__content">
+            <p>
+              I just got these Lactation cookies from Baby&Co, and I love it!
+              NB; This isn't an advert.
+            </p>
+            <i>4 hours ago by Gloria</i>
+            <aside>Open thread</aside>
+            <hr />
+          </div>
+          <li className="topicTitle">Blood In Breastmilk</li>
+          <div className="nameTag">
+            <div className="nameLogo">A</div>
+            <h6 className="posterName">By Anonymous</h6>
+          </div>
+          <div className="collapsible__content">
+            <p>
+              The breastmilk I pumped looks pink. Could it be blood? Has any one
+              experienced this? Help! I'm freaking out.
+            </p>
+            <i>21 hours ago by Anonymous</i>
+            <aside>Open thread</aside>
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default CommPostDetail;
